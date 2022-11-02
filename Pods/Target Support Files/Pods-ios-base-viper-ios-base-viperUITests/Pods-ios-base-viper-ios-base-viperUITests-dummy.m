f8e410c6286f9b8b48251a685d40f97eaa7a8d6f
Isaac Newton Tambunan <isaac.tambunan@ottodigital.id>
2022-11-02 17:30:04 +0700

    setup base

---
+++ Pods/Target Support Files/Pods-ios-base-viper-ios-base-viperUITests/Pods-ios-base-viper-ios-base-viperUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_base_viper_ios_base_viperUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_base_viper_ios_base_viperUITests
+@end
